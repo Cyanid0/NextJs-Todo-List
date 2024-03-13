b12779147192b945c95ca8bda1a6c117a2427a7b
Cyanido <adamhdhayfaumar.aids2022@citchennai.net>
2024-03-13 09:45:30 +0530

    fix: fixed unecassary useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { TodoItem } from '@/components/TodoItem';
 import prisma from '@/db'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
-async function toggleTodo(id: string, completed: boolean){
+async function toggleTodo(id: string, completed: boolean) {
   'use server'
-  await prisma.todo.update({where: {id}, data: {completed}})
-  }
+  await prisma.todo.update({ where: { id }, data: { completed } })
+}
 
-async function deleteTodo (id: string){
+async function deleteTodo(id: string) {
   'use server'
-  await prisma.todo.delete({where: {id}})
+  await prisma.todo.delete({ where: { id } })
 }
 
 export default async function Home() {
@@ -20,11 +21,16 @@ export default async function Home() {
         <span className='text-5xl'> Todo List </span>
         <Link href='/new' className='text-lg border px-3 py-1 rounded-md hover:text-black hover:bg-white transition duration-500 hover:translate-y-[-5px]'> New </Link>
       </header>
-      <ul className="m-4 border text-xl rounded-md py-2 px-3">
-        {todos.map(todo => (
+      <Suspense fallback={<div>Loading...</div>}>
+        {
+          await new Promise(resolve => setTimeout(resolve, 3000))
+        }
+        <ul className="m-4 border text-xl rounded-md py-2 px-3">
+          {todos.map(todo => (
             <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-        ))}
-      </ul>
+          ))}
+        </ul>
+      </Suspense>
     </>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import { useState, useEffect } from "react"
+import { useState, useEffect, Suspense } from "react"
 import { FaTrash, FaEdit } from 'react-icons/fa';
 type TodoItemProps = {
   id: string,
@@ -12,38 +12,36 @@ type TodoItemProps = {
 
 export function TodoItem({ id, title, completed, toggleTodo, deleteTodo }: TodoItemProps) {
 
-  const [isComplete, setIsComplete] = useState(false)
+  const [isComplete, setIsComplete] = useState(completed)
   const [isDeleted, setIsDeleted] = useState(false)
-
-  useEffect(() => {
-    setIsComplete(completed);
-  }, []);
 
   return (
     isDeleted ? null :
-    <li className="flex justify-between items-center border-b border-slate-900">
-      <div className="flex gap-4 items-center">
-        <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={completed} onChange={e => { 
-          toggleTodo(id, e.target.checked)
-          setIsComplete(e.target.checked)
-        }} />
-        <label htmlFor={id} className="peer-checked:line-through peer-checked:text-slate-500">
-          {title}
-        </label>
-      </div>
-      <div className="flex gap-3" >
-      <Link href={`/edit/${id}`} className="text-xl text-white cursor-pointer">
-        <FaEdit className="text-xl text-white cursor-pointer" />
-      </Link>
-      {isComplete && 
-        <span className="text-xl text-white cursor-pointer" onClick={() => {
-          deleteTodo(id)
-          setIsDeleted(true)
-        }}>
-          <FaTrash />
-        </span>
-      }
-      </div>
-    </li>
+      <li className="flex justify-between items-center border-b border-slate-900">
+        <div className="flex gap-4 items-center">
+          <input id={id} type="checkbox" className="cursor-pointer peer" defaultChecked={completed} onChange={e => {
+            toggleTodo(id, e.target.checked)
+            setIsComplete(e.target.checked)
+          }} />
+          <label htmlFor={id} className="peer-checked:line-through peer-checked:text-slate-500">
+            {title}
+          </label>
+        </div>
+        <div className="flex gap-3" >
+          <Link href={`/edit/${id}`} className="text-xl text-white cursor-pointer">
+            <FaEdit className="text-xl text-white cursor-pointer" />
+          </Link>
+          <Suspense fallback={<span> Loading... </span>}>
+          {isComplete &&
+            <span className="text-xl text-white cursor-pointer" onClick={() => {
+              deleteTodo(id)
+              setIsDeleted(true)
+            }}>
+              <FaTrash />
+            </span>
+          }
+          </Suspense>
+        </div>
+      </li>
   )
 }
